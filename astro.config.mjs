db6d6d47327d4d95b791b7ea58e2fdcf96f828be
Andres Ajhuacho <andres.ajhra@gmail.com>
2025-02-13 17:27:15 -0400

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.sayari-consultora.com/',
+    site: 'http://sayari-consultora.com/',
     base: '/site-sayari',
 });
