a2bf58b5bb27818d99f292612bf90a3c7a5c53ea
Andres Ajhuacho <andres.ajhra@gmail.com>
2025-02-21 16:33:22 -0400

    add page builder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://sayari-consultora.com/'
+    site: 'http://sayari-consultora.com/',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
